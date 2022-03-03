7a3dd47d0b36c6ff15b1205f3ec68e050bf593c5
Andrey Viana <64756172+andreyviana@users.noreply.github.com>
2022-03-03 08:55:17 -0300

    Add files via upload

---
+++ aula_03_03_22/exemplo_01.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<h3>Pos-Incremeno</h3>";
+    $a = 5;
+    echo "deve ser 5: " . $a++ . "<br />\n";
+    echo "deve ser 6: " . $a . "<br />\n";
+
+?>
---
+++ aula_03_03_22/exemplo_03.php
@@ -0,0 +1,12 @@
+<?php
+    $a = "10";
+    $b = 10;
+    $soma = $a + $b;
+
+    echo "usando == : ".($a==$b);
+    echo "<br>";
+    echo "usando === : ".($a===$b);
+    echo "<br>";
+    echo "somando a + b => ". $soma;
+
+?>
